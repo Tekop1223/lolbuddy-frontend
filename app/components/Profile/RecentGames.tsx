33c8b51921b389484732ce82bc351e470af6ac86
Eray Tecer <ertec@live.dk>
2024-12-08 18:43:31 +0100

    added queueId and fixed gameDuration

--- app/components/Profile/RecentGames.tsx
+++ app/components/Profile/RecentGames.tsx
@@ -1,6 +1,7 @@
 interface RecentGamesProps {
     //info.
     gameDuration: number;
+    queueId: number;
     //info.participants.
     kills: number;
     deaths: number;
@@ -11,17 +12,57 @@ interface RecentGamesProps {
     championName: string;
     visionScore: number;
 }
+// // queueId = gametype 
+// 400 = Normal Draft Pick, 420 = Ranked Solo/Duo, 430 = Normal blind pick, 440 = Ranked Flex, 450 = ARAM,
+const getGameType = (queueId: number): string => {
+    switch(queueId) {
+        case 400:
+            return "Normal Draft Pick";
+        case 420:
+            return "Ranked";
+        case 430:
+            return "Normal blind pick";
+        case 440:
+            return "Ranked Flex";
+        case 450:
+            return "ARAM";
+        default:
+            return "Unknown";
+    }
+}
+// // teamId = side of the map
+// 100 = blue side, 200 = red side
 
-export function RecentGamesCard ({gameDuration, kills, deaths, assists, kda, champLevel, championId, championName, visionScore}: RecentGamesProps) {
+
+const formatGameDuration = (gameDuration: number): string => {
+    const minutes = Math.floor(gameDuration / 60);
+    const seconds = gameDuration % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
+
+
+export function RecentGamesCard ({gameDuration, queueId, kills, deaths, assists, kda, champLevel, championId, championName, visionScore}: RecentGamesProps) {
+    const gameType = getGameType(queueId);
+    const formattedGameDuration = formatGameDuration(gameDuration);
     return (
-    <div className="p-4">
+    <div className="p-4 m-4 bg-gray-800 shadow-md rounded-lg flex items-center space-x-4">
         <div>
-            <img src={`/datadragon/14.23.1/img/champion/${championId}.png`} alt="Champion icon" className="w-12 h-12"/>
-            <h2>lv.{champLevel}, {championName}</h2>
-            <p>{gameDuration}</p>
-            <p>{kills}/{deaths}/{assists}</p>
-            <p>{kda}</p>
-            <p>{visionScore}</p>
+            <p className="text-m text-bold text-white">{gameType}</p>
+            <p className="text-sm text-gray-400 text-center">{formattedGameDuration}</p>
+        </div>
+        <div className="relative">
+            <img src={`/datadragon/14.23.1/img/champion/${championName}.png`} alt="Champion icon" className="w-16 h-16 rounded-lg"/>
+            <div className="absolute bottom-0 left-0 bg-gray-700 text-gray-200 text-xs rounded px-1">
+                {champLevel}
+            </div>
+        </div>
+        <div>
+            <h2 className="text-lg text-gray-200">{championName}</h2>
+            
+        
+            <p className="text-sm text-gray-400">K/D/A: {kills} / {deaths} / {assists}</p>
+            <p className="text-sm text-gray-400">KDA: {kda}</p>
+            <p className="text-sm text-gray-400">Vision Score: {visionScore}</p>
         </div>
     
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,43 +4,44 @@ import { RecentGamesCard } from "./components/Profile/RecentGames";
 
 export default function Home() {
   return (
-    <div className="">  
-    <div>
-      <SummonerCard
-        profileIconId={6759}
-        gameName="Tekop"
-        tagLine="tekop"
-        summonerLevel={30}
-        />
-    </div>
-    <div>
-      <RankCard
-        tier="Challenger"
-        rank=""
-        leaguePoints={2432}
-        wins={100}
-        losses={21}
-      />
-    </div>
-    <div>
-      <p>recent champions performence</p>
-    </div>
-    <div>
-    <RecentGamesCard
-        gameDuration={1460}
-        kills={999}
-        deaths={0}
-        assists={69}
-        kda={0}
-        champLevel={18}
-        championId={48}
-        championName="Trundle"
-        visionScore={420}
-      />
-    </div>
-    <div>
-      Patch notes that reflects champions pool
-    </div>
-    </div>
+      <div className="">
+          <div>
+              <SummonerCard
+                  profileIconId={6759}
+                  gameName="Tekop"
+                  tagLine="tekop"
+                  summonerLevel={30}
+              />
+          </div>
+
+          <div className="flex space-x-4">
+              <div className="flex-shrink-0">
+                  <RankCard
+                      tier="Challenger"
+                      rank=""
+                      leaguePoints={2432}
+                      wins={100}
+                      losses={21}
+                  />
+              </div>
+              <div className="flex-grow">
+                  <RecentGamesCard
+                      gameDuration={1445}
+                      queueId={420}
+                      kills={999}
+                      deaths={0}
+                      assists={69}
+                      kda={0}
+                      champLevel={18}
+                      championId={48}
+                      championName="Trundle"
+                      visionScore={420}
+                  />
+              </div>
+          </div>
+          <div className="mt-4">
+              <p>Recent Champion Performance</p>
+          </div>
+      </div>
   );
 }
